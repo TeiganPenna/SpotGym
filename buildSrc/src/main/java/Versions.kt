53147ac38494df0492295db1a2cdd85bbc4b3b17
TeiganPenna <teigan.penna@gmail.com>
2022-08-17 17:15:48 +1000

    Use gradle kotlin (#9)
    * use double quotes
    
    * disambiguate function invocations and property assignments
    
    * migrate settings.gradle to kotlin
    
    * migrate build.gradle to kotlin

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,18 @@
+object Versions {
+    // Sdk and tools
+    const val COMPILE_SDK = 32
+    const val MIN_SDK = 30
+    const val TARGET_SDK = 30
+
+    // App dependencies
+    const val ACTIVITY_COMPOSE = "1.5.1"
+    const val COMPOSE = "1.2.1"
+    const val CORE_TESTING = "2.1.0"
+    const val ESPRESSO = "3.4.0"
+    const val JUNIT = "4.13.2"
+    const val KTX = "1.8.0"
+    const val LIFECYCLE = "2.5.1"
+    const val NAVIGATION_COMPOSE = "2.5.1"
+    const val ROOM = "2.4.3"
+    const val TEST_EXT_JUNIT = "1.1.3"
+}
