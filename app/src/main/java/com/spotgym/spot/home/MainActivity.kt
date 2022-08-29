d9adafb3a29d4d11eeee8f480d6d82f1930d1506
Teigan Penna <teigan.penna@gmail.com>
2022-08-29 18:13:15 +1000

    Move the common scaffold and surface out of the routine and home page component

--- app/src/main/java/com/spotgym/spot/home/MainActivity.kt
+++ app/src/main/java/com/spotgym/spot/home/MainActivity.kt
@@ -3,9 +3,19 @@ package com.spotgym.spot.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.spotgym.spot.R
 import com.spotgym.spot.ui.theme.SpotTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,19 +24,37 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             SpotTheme {
-                val navController = rememberNavController()
-
-                NavHost(navController = navController, startDestination = Routes.Home.route) {
-                    composable(Routes.Home.route) {
-                        SpotHome(
-                            onRoutineClicked = { routineId ->
-                                navController.navigate(Routes.Routine.route + "/$routineId")
-                            }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(stringResource(R.string.routines_title)) }
                         )
                     }
-                    composable(Routes.Routine.route + "/{routineId}") { backStackEntry ->
-                        val routineId = backStackEntry.arguments?.getString("routineId")
-                        RoutinePage(routineId!!)
+                ) { contentPadding ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(contentPadding),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        val navController = rememberNavController()
+
+                        NavHost(
+                            navController = navController,
+                            startDestination = Routes.Home.route
+                        ) {
+                            composable(Routes.Home.route) {
+                                SpotHome(
+                                    onRoutineClicked = { routineId ->
+                                        navController.navigate(Routes.Routine.route + "/$routineId")
+                                    }
+                                )
+                            }
+                            composable(Routes.Routine.route + "/{routineId}") { backStackEntry ->
+                                val routineId = backStackEntry.arguments?.getString("routineId")
+                                RoutinePage(routineId!!)
+                            }
+                        }
                     }
                 }
             }
--- app/src/main/java/com/spotgym/spot/home/SpotHome.kt
+++ app/src/main/java/com/spotgym/spot/home/SpotHome.kt
@@ -2,22 +2,15 @@ package com.spotgym.spot.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.spotgym.spot.R
 
 typealias OnRoutineClicked = (String) -> Unit
 
@@ -25,41 +18,26 @@ typealias OnRoutineClicked = (String) -> Unit
 fun SpotHome(
     onRoutineClicked: OnRoutineClicked
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(stringResource(R.string.routines_title)) }
-            )
-        }
-    ) { contentPadding ->
-        Surface(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(contentPadding),
-            color = MaterialTheme.colors.background
-        ) {
-            Column(modifier = Modifier.padding(10.dp)) {
-                RoutineCard(
-                    name = "Day A",
-                    description = "Chest day: Bench press, Flies, and Dumbbell curls",
-                    onRoutineClicked = onRoutineClicked
-                )
-                RoutineCard(
-                    name = "Day B", description = "Back day: Pull ups, Rows, and Leg press",
-                    onRoutineClicked = onRoutineClicked
-                )
-                RoutineCard(
-                    name = "Day C",
-                    description = "Triceps day: Bench press, Extensions, and Flies",
-                    onRoutineClicked = onRoutineClicked
-                )
-                RoutineCard(
-                    name = "Day D",
-                    description = "Leg day: Squats, Deadlifts, and Leg press",
-                    onRoutineClicked = onRoutineClicked
-                )
-            }
-        }
+    Column(modifier = Modifier.padding(10.dp)) {
+        RoutineCard(
+            name = "Day A",
+            description = "Chest day: Bench press, Flies, and Dumbbell curls",
+            onRoutineClicked = onRoutineClicked
+        )
+        RoutineCard(
+            name = "Day B", description = "Back day: Pull ups, Rows, and Leg press",
+            onRoutineClicked = onRoutineClicked
+        )
+        RoutineCard(
+            name = "Day C",
+            description = "Triceps day: Bench press, Extensions, and Flies",
+            onRoutineClicked = onRoutineClicked
+        )
+        RoutineCard(
+            name = "Day D",
+            description = "Leg day: Squats, Deadlifts, and Leg press",
+            onRoutineClicked = onRoutineClicked
+        )
     }
 }
 
