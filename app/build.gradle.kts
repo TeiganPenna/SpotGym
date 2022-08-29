f8027a7ea58c26643ded2ed3fbc6ff094581755c
TeiganPenna <teigan.penna@gmail.com>
2022-08-29 18:46:31 +1000

    11 fix screen flashing (#12)
    * Move the common scaffold and surface out of the routine and home page component
    
    * Use a viewmodel to change the title of the scaffold
    
    * Bump the version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.spotgym.spot"
         minSdk = Versions.MIN_SDK
         targetSdk = Versions.TARGET_SDK
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
