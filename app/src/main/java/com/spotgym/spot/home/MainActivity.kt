1a0ae96eeb14510b4e46c7a36cbfc35ef2297c96
TeiganPenna <teigan.penna@gmail.com>
2022-08-29 18:46:31 +1000

    11 fix screen flashing (#12)
    * Move the common scaffold and surface out of the routine and home page component
    
    * Use a viewmodel to change the title of the scaffold

--- app/src/main/java/com/spotgym/spot/home/MainActivity.kt
+++ app/src/main/java/com/spotgym/spot/home/MainActivity.kt
@@ -3,6 +3,19 @@ package com.spotgym.spot.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -13,12 +26,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            SpotTheme {
+            LoadMainScreen()
+        }
+    }
+}
+
+@Composable
+fun LoadMainScreen(mainViewModel: MainViewModel = viewModel()) {
+    val title: String by mainViewModel.screenTitle.observeAsState("")
+
+    SpotTheme {
+        Scaffold(
+            scaffoldState = rememberScaffoldState(),
+            topBar = {
+                TopAppBar(
+                    title = { Text(title) },
+                )
+            }
+        ) { contentPadding ->
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(contentPadding),
+                color = MaterialTheme.colors.background
+            ) {
                 val navController = rememberNavController()
 
-                NavHost(navController = navController, startDestination = Routes.Home.route) {
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.Home.route
+                ) {
                     composable(Routes.Home.route) {
                         SpotHome(
+                            viewModel = mainViewModel,
                             onRoutineClicked = { routineId ->
                                 navController.navigate(Routes.Routine.route + "/$routineId")
                             }
@@ -26,7 +66,7 @@ class MainActivity : ComponentActivity() {
                     }
                     composable(Routes.Routine.route + "/{routineId}") { backStackEntry ->
                         val routineId = backStackEntry.arguments?.getString("routineId")
-                        RoutinePage(routineId!!)
+                        RoutinePage(mainViewModel, routineId!!)
                     }
                 }
             }
