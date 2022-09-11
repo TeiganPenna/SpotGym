72ccc2c97d611beaab3d625b0ea8194b11cff2df
Teigan Penna <teigan.penna@gmail.com>
2022-09-11 18:53:36 +1000

    Extract dialog composable to its own file

---
+++ app/src/main/java/com/spotgym/spot/home/SpotDialog.kt
@@ -0,0 +1,122 @@
+package com.spotgym.spot.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.spotgym.spot.R
+
+@Composable
+@ExperimentalComposeUiApi
+fun SpotDialog(
+    title: String,
+    setShowDialog: (Boolean) -> Unit,
+    onPositiveClick: () -> Unit,
+    validate: () -> ValidationResult,
+    content: @Composable
+    () -> Unit,
+) {
+    var errorField by remember { mutableStateOf("") }
+
+    Dialog(
+        onDismissRequest = { setShowDialog(false) },
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        Card(
+            elevation = 10.dp,
+            modifier = Modifier
+                .padding(35.dp)
+                .fillMaxWidth()
+                .wrapContentWidth()
+        ) {
+            Column(
+                modifier = Modifier.padding(15.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Text(text = title, fontWeight = FontWeight.Bold)
+
+                content()
+
+                if (errorField.isNotBlank()) {
+                    Text(text = errorField, color = colorResource(R.color.error_red))
+                }
+
+                DialogResultButtons(
+                    onNegativeClick = { setShowDialog(false) }
+                ) {
+                    val result = validate()
+                    if (result.isSuccess) {
+                        onPositiveClick()
+                        setShowDialog(false)
+                    } else {
+                        errorField = result.message ?: ""
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DialogValidationTextField(
+    label: String,
+    value: MutableState<String>,
+    isError: MutableState<Boolean>,
+) {
+    TextField(
+        label = { Text(label) },
+        value = value.value,
+        onValueChange = {
+            value.value = it
+            isError.value = false
+        },
+        isError = isError.value,
+        singleLine = true,
+    )
+}
+
+data class ValidationResult(
+    val isSuccess: Boolean,
+    val message: String?,
+)
+
+@Composable
+private fun DialogResultButtons(
+    onNegativeClick: () -> Unit,
+    onPositiveClick: () -> Unit,
+) {
+    Row(
+        horizontalArrangement = Arrangement.End,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TextButton(onClick = onNegativeClick) {
+            Text(text = stringResource(android.R.string.cancel))
+        }
+        Spacer(modifier = Modifier.width(3.dp))
+        TextButton(onClick = onPositiveClick) {
+            Text(text = stringResource(android.R.string.ok))
+        }
+    }
+}
--- app/src/main/java/com/spotgym/spot/home/SpotHome.kt
+++ app/src/main/java/com/spotgym/spot/home/SpotHome.kt
@@ -1,18 +1,11 @@
 package com.spotgym.spot.home
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import android.content.Context
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
@@ -22,28 +15,24 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import com.spotgym.spot.R
 import com.spotgym.spot.data.Routine
 import kotlinx.coroutines.launch
@@ -65,9 +54,10 @@ fun SpotHome(
 
     val showAddDialog = remember { mutableStateOf(false) }
     if (showAddDialog.value) {
-        AddRoutineDialog(setShowDialog = { showAddDialog.value = it }) {
-            addRoutine(it)
-        }
+        AddRoutineDialog(
+            showDialog = showAddDialog,
+            onPositiveClick = { addRoutine(it) }
+        )
     }
 
     Scaffold(
@@ -102,6 +92,43 @@ fun SpotHome(
 }
 
 @Composable
+@ExperimentalComposeUiApi
+private fun AddRoutineDialog(
+    showDialog: MutableState<Boolean>,
+    onPositiveClick: (Routine) -> Unit,
+) {
+    val context = LocalContext.current
+
+    val name = remember { mutableStateOf("") }
+    val nameIsError = remember { mutableStateOf(false) }
+
+    val description = remember { mutableStateOf("") }
+    val descriptionIsError = remember { mutableStateOf(false) }
+
+    SpotDialog(
+        title = stringResource(R.string.routines_name_routine),
+        setShowDialog = { showDialog.value = it },
+        validate = { validateRoutine(context, name, nameIsError, description, descriptionIsError) },
+        onPositiveClick = {
+            val routine = Routine(name = name.value, description = description.value)
+            onPositiveClick(routine)
+        },
+    ) {
+        DialogValidationTextField(
+            label = stringResource(R.string.routine_field_name),
+            value = name,
+            isError = nameIsError
+        )
+
+        DialogValidationTextField(
+            label = stringResource(R.string.routine_field_desc),
+            value = description,
+            isError = descriptionIsError
+        )
+    }
+}
+
+@Composable
 private fun RoutineCard(
     routine: Routine,
     onRoutineClicked: OnRoutineClicked
@@ -131,82 +158,37 @@ private fun RoutineCard(
     }
 }
 
-@Composable
-@ExperimentalComposeUiApi
-private fun AddRoutineDialog(
-    setShowDialog: (Boolean) -> Unit,
-    onPositiveClick: (Routine) -> Unit,
-) {
-    var name by remember { mutableStateOf("") }
-    var description by remember { mutableStateOf("") }
-    var errorField by remember { mutableStateOf("") }
-
-    Dialog(
-        onDismissRequest = { setShowDialog(false) },
-        properties = DialogProperties(usePlatformDefaultWidth = false)
-    ) {
-        Card(
-            elevation = 10.dp,
-            modifier = Modifier
-                .padding(35.dp)
-                .fillMaxWidth()
-                .wrapContentWidth()
-        ) {
-            Column(
-                modifier = Modifier.padding(15.dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                Text(
-                    text = stringResource(R.string.routines_name_routine),
-                    fontWeight = FontWeight.Bold
-                )
-
-                TextField(
-                    value = name,
-                    onValueChange = { name = it },
-                    label = { Text(stringResource(R.string.routine_field_name)) }
-//                    modifier = Modifier.border(
-//                        BorderStroke(
-//                            width = 2.dp,
-//                            color = colorResource(id = if (errorField.isEmpty()) R.color.error_red else R.color.black)
-//                        )
-//                    ) // TODO make border red when error
-                )
-
-                TextField(
-                    value = description,
-                    onValueChange = { description = it },
-                    label = { Text(stringResource(R.string.routine_field_desc)) }
-                )
-
-                if (errorField.isNotBlank()) {
-                    Text(text = errorField, color = colorResource(R.color.error_red))
-                }
-
-                Row(
-                    horizontalArrangement = Arrangement.End,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    TextButton(onClick = { setShowDialog(false) }) {
-                        Text(text = stringResource(R.string.dialog_button_cancel))
-                    }
-                    Spacer(modifier = Modifier.width(3.dp))
-                    TextButton(onClick = {
-                        if (name.isBlank()) {
-                            errorField = "Name cannot be empty"
-                            return@TextButton
-                        }
-                        if (description.isBlank()) {
-                            errorField = "Description cannot be empty"
-                            return@TextButton
-                        }
-                        onPositiveClick(Routine(name = name, description = description))
-                        setShowDialog(false)
-                    }) {
-                        Text(text = stringResource(R.string.dialog_button_ok))
-                    }
-                }
-            }
-        }
+@SuppressWarnings("ReturnCount")
+private fun validateRoutine(
+    context: Context,
+    name: MutableState<String>,
+    nameIsError: MutableState<Boolean>,
+    description: MutableState<String>,
+    descriptionIsError: MutableState<Boolean>,
+): ValidationResult {
+    if (name.value.isBlank()) {
+        nameIsError.value = true
+        return ValidationResult(
+            false,
+            context.getString(
+                R.string.routine_validation_empty,
+                context.getString(R.string.routine_name)
+            )
+        )
+    } else {
+        nameIsError.value = false
     }
+    if (description.value.isBlank()) {
+        descriptionIsError.value = true
+        return ValidationResult(
+            false,
+            context.getString(
+                R.string.routine_validation_empty,
+                context.getString(R.string.routine_description)
+            )
+        )
+    } else {
+        descriptionIsError.value = false
+    }
+    return ValidationResult(true, null)
 }
