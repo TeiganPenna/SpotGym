73773236cefc1e6ed6c1e5c9ab4ca240667c9fea
Teigan Penna <teigan.penna@gmail.com>
2022-10-02 23:01:18 +1100

    SpotDismissibleCard allows for swiping and deleting

---
+++ app/src/androidTest/java/com/spotgym/spot/home/SpotDismissibleCardTest.kt
@@ -0,0 +1,89 @@
+package com.spotgym.spot.home
+
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeLeft
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalMaterialApi
+class SpotDismissibleCardTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun `displays content text`() {
+        setUpCard()
+
+        composeTestRule.onNodeWithText("Some text").assertIsDisplayed().assertHasClickAction()
+    }
+
+    @Test
+    fun `content is clickable`() {
+        var clicked = false
+        setUpCard(onCardClicked = { clicked = true })
+
+        composeTestRule.onNodeWithText("Some text").performClick()
+        assertThat(clicked).isTrue
+    }
+
+    @Test
+    fun `when swiped shows delete confirmation dialog`() {
+        setUpCard()
+
+        composeTestRule.onNodeWithText("Some text").performTouchInput { swipeLeft() }
+
+        composeTestRule.onNodeWithText("Some title").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Some body").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Cancel").assertIsDisplayed().assertHasClickAction()
+        composeTestRule.onNodeWithText("Delete").assertIsDisplayed().assertHasClickAction()
+    }
+
+    @Test
+    fun `when delete is cancelled then no dismiss`() {
+        var dismissed = false
+        setUpCard(onDismissed = { dismissed = true })
+
+        composeTestRule.onNodeWithText("Some text").performTouchInput { swipeLeft() }
+
+        composeTestRule.onNodeWithText("Cancel").performClick()
+
+        assertThat(dismissed).isFalse
+    }
+
+    @Test
+    fun `when delete is confirmed then dismiss`() {
+        var dismissed = false
+        setUpCard(onDismissed = { dismissed = true })
+
+        composeTestRule.onNodeWithText("Some text").performTouchInput { swipeLeft() }
+
+        composeTestRule.onNodeWithText("Delete").performClick()
+
+        assertThat(dismissed).isTrue
+    }
+
+    private fun setUpCard(
+        onCardClicked: () -> Unit = {},
+        onDismissed: () -> Unit = {}
+    ) {
+        composeTestRule.setContent {
+            SpotDismissibleCard(
+                onCardClicked = onCardClicked,
+                onDismissed = onDismissed,
+                confirmTitle = "Some title",
+                confirmBody = "Some body"
+            ) {
+                Text(text = "Some text")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/spotgym/spot/home/SpotDismissibleCard.kt
@@ -0,0 +1,120 @@
+package com.spotgym.spot.home
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Card
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.SwipeToDismiss
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberDismissState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.spotgym.spot.R
+
+@Composable
+@ExperimentalMaterialApi
+fun SpotDismissibleCard(
+    onCardClicked: () -> Unit,
+    onDismissed: () -> Unit,
+    confirmTitle: String,
+    confirmBody: String,
+    modifier: Modifier = Modifier,
+    content: @Composable
+    () -> Unit,
+) {
+    var showDismissAlert by remember { mutableStateOf(false) }
+    if (showDismissAlert) {
+        DismissRoutineAlert(
+            setShowAlert = { showDismissAlert = it },
+            onConfirmed = onDismissed,
+            confirmTitle = confirmTitle,
+            confirmBody = confirmBody,
+        )
+    }
+
+    val dismissState = rememberDismissState(
+        confirmStateChange = {
+            showDismissAlert = true
+            false // always disable, this is the only way to have an effective confirmation dialog
+        }
+    )
+
+    SwipeToDismiss(
+        state = dismissState,
+        directions = setOf(DismissDirection.EndToStart),
+        modifier = modifier,
+        background = {
+            Box(
+                contentAlignment = Alignment.CenterEnd,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 10.dp)
+            ) {
+                Icon(
+                    Icons.Default.Delete,
+                    contentDescription = stringResource(R.string.spot_card_delete)
+                )
+            }
+        }
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    onCardClicked()
+                },
+            elevation = 10.dp
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+private fun DismissRoutineAlert(
+    setShowAlert: (Boolean) -> Unit,
+    onConfirmed: () -> Unit,
+    confirmTitle: String,
+    confirmBody: String,
+) {
+    AlertDialog(
+        onDismissRequest = { setShowAlert(false) },
+        title = {
+            Text(text = confirmTitle)
+        },
+        text = {
+            Text(text = confirmBody)
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    setShowAlert(false)
+                    onConfirmed()
+                }
+            ) {
+                Text(text = stringResource(R.string.delete))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { setShowAlert(false) }) {
+                Text(text = stringResource(android.R.string.cancel))
+            }
+        }
+    )
+}
