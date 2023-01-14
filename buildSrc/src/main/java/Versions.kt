9f56c5e22a628a62d5a49370976f7ab6e57d1c03
Teigan Penna <teigan.penna@gmail.com>
2023-01-14 19:29:36 +1100

    update mockk

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -31,7 +31,7 @@ object Versions {
     const val JUNIT_JUPITER_ANDROID = "1.3.0"
     const val JUNIT_JUPITER = "5.9.1"
     const val KOTLINX_COROUTINES = "1.6.4"
-    const val MOCKK = "1.13.2"
+    const val MOCKK = "1.13.3"
     const val TEST_EXT_JUNIT = "1.1.5"
 
     fun isStable(version: String): Boolean {
