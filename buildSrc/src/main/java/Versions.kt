b68e62e03673660fb097bfece8e7c78dc4d232ff
Teigan Penna <teigan.penna@gmail.com>
2023-01-14 18:42:16 +1100

    update compose_material

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -12,7 +12,7 @@ object Versions {
     const val ACCOMPANIST = "0.25.1"
     const val ACTIVITY_COMPOSE = "1.6.1"
     const val COMPOSE = "1.3.2"
-    const val COMPOSE_MATERIAL = "1.2.1"
+    const val COMPOSE_MATERIAL = "1.3.1"
     const val COMPOSE_REORDERABLE = "0.9.2"
     const val COMPOSE_RUNTIME = "1.2.1"
     const val COMPOSE_UI = "1.2.1"
